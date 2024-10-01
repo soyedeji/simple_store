bf15902760f399df46bd0298a3b088be41bf25f4
Sam <soyedeji@academic.rrc.ca>
2024-10-01 12:18:49 -0500

    create product.csv, modify seed.rb to load csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-Product.destroy_all
+require "csv"
 
-676.times do
+# Clear out existing products and categories
+Product.destroy_all
+Category.destroy_all
+
+# Read the CSV file
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product_row|
+  # Find or create the category
+  category_name = product_row['category_name']
+  category = Category.find_or_create_by(name: category_name)
+
+  # Create the product and associate it with the category
   Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Commerce.price(range: 10.0..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+    title: product_row['title'],
+    description: product_row['description'],
+    price: product_row['price'],
+    stock_quantity: product_row['stock_quantity'],
+    category: category
   )
 end
